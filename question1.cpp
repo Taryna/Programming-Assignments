b7f9453a24ebffa34458c78990edfb5911158fee
Taryna <taryna2012@gmail.com>
2013-09-30 15:56:21 -0400

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,21 @@
+int main()
+{
+    cout << "Enter #1: ";
+    double a;
+    cin>>a;
+    cout << "Enter #2: ";
+    double b;
+    cin>>b;
+    cout << "Enter #3: ";
+    double c;
+    cin>>c;
+    cout<<"Enter #4: ";
+    double d;
+    cin>>d;
+    cout<<"Enter #5: ";
+    double e;
+    cin>> e;
+    cout<<"The sum is "<<a+b+c+d+e<<endl;
+
+    return 0;
+}
